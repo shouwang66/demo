5989238e9b2a1c094c452da85cfc24173f7ad913
nihanlong <8427523aa@qq.com>
2018-08-24 17:49:44 +0800

    111

---
+++ src/components/BFixedPay/index.vue
@@ -0,0 +1,74 @@
+<template>
+<div class="b-nav">
+  <div class="b-n-box br1px brt1px">
+    <p class="summary">
+      <span class="tit">合计</span>
+      <span class="money">
+        <em class="icon">￥</em> {{ paySummary }}
+      </span>
+    </p>
+    <button class="b-sure" type="submit">{{ payTxt }}</button>
+  </div>
+</div>
+</template>
+<script>
+export default {
+  name: 'pay',
+  props: ['paySummary', 'payTxt']
+}
+</script>
+<style lang="less" scoped>
+.b-nav {
+  height: 98px;
+  .b-n-box {
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    height: 98px;
+    display: flex;
+    background-color: #fff;
+    .summary {
+      flex: 3;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      .tit {
+        font-size: 28px;
+        color: #a4a4a4;
+      }
+      .money {
+        display: flex;
+        align-items: center;
+        font-size: 32px;
+        font-weight: bold;
+        color: #ff7031;
+        .icon {
+          margin: 0 15px;
+        }
+      }
+    }
+    .b-sure {
+      flex: 4;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      font-size: 32px;
+      color: #fff;
+      background-color: #ff7031;
+    }
+  }
+}
+@media only screen and (device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3) {
+  .b-nav {
+    height: 128px;
+    .b-n-box {
+      height: 128px;
+      .summary,
+      .b-sure {
+        padding-bottom: 30px;
+      }
+    }
+  }
+}
+</style>
--- src/pages/ActivityApply/index.vue
+++ src/pages/ActivityApply/index.vue
@@ -10,103 +10,108 @@
       <p class="time"><span>报名时间</span><span>2016年11月01日 —11月30日</span></p>
     </div>
   </div>
-
-  <div class="apply-content">
-    <div class="a-c-row mt20 bg-f">
-      <p class="title"><em>学生信息</em></p>
-      <div class="row mt20 br1px brb1px">
-        <span class="tit">学生姓名</span>
-        <div class="ipt">
-          <input class="h75 tr" type="text" disabled v-model="name">
-        </div>
-      </div>
-      <div class="row">
-        <span class="tit spacing"><em>性</em><em>别</em></span>
-        <div class="ipt">
-          <input class="h75 tr" type="text" disabled v-model="sex">
-        </div>
-      </div>
-    </div>
-
-    <div class="a-c-row mt20 bg-f">
-      <p class="title"><em>报考信息</em></p>
-      <div class="r-box br1px brb1px">
-        <div class="row mt20">
-          <span class="tit">选择校点</span>
-          <div class="ipt sel-ipt">
-            <select name="" class="h75">
-              <option value="">上海圆桌巨峰路校</option>
-              <option value="">上海圆桌巨峰路校</option>
-              <option value="">上海圆桌巨峰路校</option>
-            </select>
+  <form action="" @submit.prevent>
+    <div class="apply-content">
+      <div class="a-c-row mt20 bg-f">
+        <p class="title"><em>学生信息</em></p>
+        <div class="row mt20 br1px brb1px">
+          <span class="tit">学生姓名</span>
+          <div class="ipt">
+            <input class="h75 tr" type="text" disabled v-model="name">
           </div>
         </div>
         <div class="row">
-          <span class="tit">报名场次</span>
-          <div class="ipt sel-ipt">
-            <select name="" class="h75">
-              <option value="">幼儿初级口语（适用于K1-2）</option>
-              <option value="">幼儿初级口语（适用于K1-2）</option>
-              <option value="">幼儿初级口语（适用于K1-2）</option>
-            </select>
+          <span class="tit spacing"><em>性</em><em>别</em></span>
+          <div class="ipt">
+            <input class="h75 tr" type="text" disabled v-model="sex">
           </div>
         </div>
       </div>
-      <div class="r-box br1px brb1px">
-        <div class="row mt20">
-          <span class="tit spacing"><em>数</em><em>量</em></span>
-          <div class="ipt">
-            <Counter :counter="counter" @getCounter="getChildCounter"/>
+
+      <div class="a-c-row mt20 bg-f">
+        <p class="title"><em>报考信息</em></p>
+        <div class="r-box br1px brb1px">
+          <div class="row mt20">
+            <span class="tit">选择校点</span>
+            <div class="ipt sel-ipt">
+              <select name="" class="h75">
+                <option value="">上海圆桌巨峰路校</option>
+                <option value="">上海圆桌巨峰路校</option>
+                <option value="">上海圆桌巨峰路校</option>
+              </select>
+            </div>
+          </div>
+          <div class="row">
+            <span class="tit">报名场次</span>
+            <div class="ipt sel-ipt">
+              <select name="" class="h75">
+                <option value="">幼儿初级口语（适用于K1-2）</option>
+                <option value="">幼儿初级口语（适用于K1-2）</option>
+                <option value="">幼儿初级口语（适用于K1-2）</option>
+              </select>
+            </div>
           </div>
         </div>
-        <div class="row muti-row">
-          <span class="tit">多选样式</span>
-          <div class="ipt">
-            <p class="chk-item">
-              <span class="chk-box"><input id="test1" type="checkbox" name="chk"><em class="chk-bg"></em></span>
-              <label class="txt" for="test1">幼儿初级口语（适用于K1-2）</label>
-            </p>
-            <p class="chk-item">
-              <span class="chk-box"><input id="test2" type="checkbox" name="chk"><em class="chk-bg"></em></span>
-              <label class="txt" for="test2">幼儿进阶口语（适用于K3）</label>
-            </p>
-            <p class="chk-item">
-              <span class="chk-box"><input id="test3" type="checkbox" name="chk"><em class="chk-bg"></em></span>
-              <label class="txt" for="test3">少儿初级口语（适用于J1）</label>
-            </p>
-            <p class="chk-item">
-              <span class="chk-box"><input id="test4" type="checkbox" name="chk"><em class="chk-bg"></em></span>
-              <label class="txt" for="test4">少儿精修口语</label>
-            </p>
+        <div class="r-box br1px brb1px">
+          <div class="row mt20">
+            <span class="tit spacing"><em>数</em><em>量</em></span>
+            <div class="ipt">
+              <Counter :counter="counter" @getCounter="getChildCounter"/>
+            </div>
+          </div>
+          <div class="row muti-row">
+            <span class="tit">多选样式</span>
+            <div class="ipt">
+              <p class="chk-item">
+                <span class="chk-box"><input id="test1" type="checkbox" name="chk"><em class="chk-bg"></em></span>
+                <label class="txt" for="test1">幼儿初级口语（适用于K1-2）</label>
+              </p>
+              <p class="chk-item">
+                <span class="chk-box"><input id="test2" type="checkbox" name="chk"><em class="chk-bg"></em></span>
+                <label class="txt" for="test2">幼儿进阶口语（适用于K3）</label>
+              </p>
+              <p class="chk-item">
+                <span class="chk-box"><input id="test3" type="checkbox" name="chk"><em class="chk-bg"></em></span>
+                <label class="txt" for="test3">少儿初级口语（适用于J1）</label>
+              </p>
+              <p class="chk-item">
+                <span class="chk-box"><input id="test4" type="checkbox" name="chk"><em class="chk-bg"></em></span>
+                <label class="txt" for="test4">少儿精修口语</label>
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="r-box">
+          <div class="row mt20">
+            <span class="tit">单行输入</span>
+            <div class="ipt">
+              <input type="text" class="h75">
+            </div>
+          </div>
+          <div class="row muti-row">
+            <span class="tit">多行输入</span>
+            <div class="ipt">
+              <textarea name=""></textarea>
+            </div>
           </div>
         </div>
       </div>
-      <div class="r-box">
-        <div class="row mt20">
-          <span class="tit">单行输入</span>
-          <div class="ipt">
-            <input type="text" class="h75">
-          </div>
-        </div>
-        <div class="row muti-row">
-          <span class="tit">多行输入</span>
-          <div class="ipt">
-            <textarea name=""></textarea>
-          </div>
-        </div>
-      </div>
+      <BottomPay :paySummary="paySummary" payTxt="确认报名" />
     </div>
-  </div>
+  </form>
 </div>
 </template>
 <script>
 import Hot from '@/components/Hot/index'
 import Counter from '@/components/Counter/index'
+import BottomPay from '@/components/BFixedPay/index'
+
 export default {
   name: 'activi-apply',
   data () {
     return {
       counter: 1,
+      paySummary: 0,
       name: 'test',
       sex: '男'
     }
@@ -119,7 +124,8 @@ export default {
   },
   components: {
     Hot,
-    Counter
+    Counter,
+    BottomPay
   }
 }
 </script>
